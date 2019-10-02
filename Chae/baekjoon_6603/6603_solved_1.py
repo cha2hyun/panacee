d5011f60fb7e9c368cc45ff25fd410bc75a86869
cha2hyun_dev <soohyun5939@icloud.com>
2019-10-02 23:28:58 +0900

    soohyun

--- Chae/baekjoon_6603/6603_solved_1.py
+++ Chae/baekjoon_6603/6603_solved_1.py
@@ -8,24 +8,28 @@ S = input().split()
 # S의 첫번째 요소를 pop 해서 K에 저장한다. pop 을 했기 때문에 리스트에서 삭제됨
 K = int(S.pop(0))
 
-# 예외처리 체크하는 변수선언
-flag = True
+while True:
+    # 탈출문
+    if K == 0: break
 
-# 예외처리 1
-# 배열의 수가 6이하거나 K가 배열의 수랑 다르면 중지
-if K < 6 or K != len(S): flag = False
+    # 예외처리 체크하는 변수선언
+    flag = True
 
-# 예외처리 2
-# 1 ~ 49 인지 확인
-# 리스트 S 를 정수형으로 변환하여 Temp 에 저장하여 비교
-Temp = list(map(int, S))
-for i in Temp:
-    if i < 1 or i > 50:
-        flag = False
+    # 예외처리 1
+    # 배열의 수가 6이하거나 K가 배열의 수랑 다르면 중지
+    if K < 6 or K != len(S): flag = False
 
-# 오류 없을때 실행
-# 리스트 S 에 있는 원소들중 6개로 이루어진 조합을 출력
-if flag:
-    for S in itertools.combinations(S, 6):
-        print(" ".join(S))
+    # 예외처리 2
+    # 1 ~ 49 인지 확인
+    # 리스트 S 를 정수형으로 변환하여 Temp 에 저장하여 비교
+    Temp = list(map(int, S))
+    for i in Temp:
+        if i < 1 or i > 50:
+            flag = False
+
+    # 오류 없을때 실행
+    # 리스트 S 에 있는 원소들중 6개로 이루어진 조합을 출력
+    if flag:
+        for S in itertools.combinations(S, 6):
+            print(" ".join(S))
 
