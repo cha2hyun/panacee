fc96117ca62512f33f5728782b2fae884d9d93dd
cha2hyun_dev <soohyun5939@icloud.com>
2019-10-30 23:14:55 +0900

    soohyun

--- Chae/Practice.py
+++ Chae/Practice.py
@@ -1,6 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-print(indeed_soup)
+print(test)
