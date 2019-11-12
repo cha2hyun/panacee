26130826acf15359291c41c271b3e6d6486d8d48
Dongkyun <leekyun0610@gmail.com>
2019-11-12 17:08:12 +0900

    Dongkyun

---
+++ kyun/10844.py
@@ -0,0 +1,17 @@
+n = int(input())
+dp = []
+
+if n >= 1:
+    for i in range(10):
+        dp[i].append(1)
+
+
+for i in range(0,10):
+    if i == 0:
+        dp[i] = i-1
+    if i == 9:
+        dp[i] = dp[]
+    else:
+
+
+
