9dff5ecf4686c6405b1ca82b5f786ec1cee54ec9
Dongkyun <leekyun0610@gmail.com>
2019-11-12 17:35:12 +0900

    Dongkyun

--- kyun/10844.py
+++ kyun/10844.py
@@ -1,17 +1,25 @@
 n = int(input())
-dp = []
+dp = [[0 for x in range(10)] for y in range(n)]
+print(dp)
 
 if n >= 1:
-    for i in range(10):
-        dp[i].append(1)
+    for j in range(n):
+        for i in range(10):
+               dp[j][i] = 1
+print(dp)
 
+for i in range(1, n):
+    for j in range(0, 10):
+        if j == 0:
+            dp[i][j] = j - 1
+        if j == 9:
+            dp[i][j] = dp[i - 1][j - 1]
+        else:
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
 
-for i in range(0,10):
-    if i == 0:
-        dp[i] = i-1
-    if i == 9:
-        dp[i] = dp[]
-    else:
+sum = int(0)
 
+for i in range(1,10):
+    sum = sum + dp[n-1][i]
 
-
+print(sum)
